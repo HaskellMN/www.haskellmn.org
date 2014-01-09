05e7b147e38955f9f33220b6de7c7c285dc81656
tippenein <tippenein@gmail.com>
2014-01-09 16:37:59 -0600

    placeholder commit

---
+++ README.md
@@ -0,0 +1,4 @@
+HaskellMN site
+====
+
+The Haskell MN site written with Hakyll for information on meet ups and such.
---
+++ src/Main.hs
@@ -0,0 +1,8 @@
+-- | Main entry point to HaskellMN-site.
+--
+
+module Main where
+
+-- | Main entry point.
+main :: IO ()
+main = return ()
