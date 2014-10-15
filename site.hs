dc7319e90e2544aefa4d120b220a712d47170f8b
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2014-10-15 18:35:31 -0500

    Tweak styling

--- site.hs
+++ site.hs
@@ -47,7 +47,7 @@ main = hakyllWith config $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
+                    constField "title" "Posts"               `mappend`
                     defaultContext
 
             makeItem ""
